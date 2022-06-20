307b2fe822c7f63c909576f60445bfee7c3f59da
SantiG95 <santiguerra86@gmail.com>
2022-06-20 14:56:41 -0300

    Initial commit

---
+++ src/Movie/Movie.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import {API_URL, API_KEY} from "../config";
+
+
+class Movie extends Component {
+
+}
+
+export default Movie;
---
+++ src/elements/header/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "./header.css"
+
+const Header = () => {
+
+}
+
+export default Header;
